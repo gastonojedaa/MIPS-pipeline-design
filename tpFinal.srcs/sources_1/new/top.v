b614f5417248e79632c0414745cc70cdf3faef23
gastonojedaa <gastonojeda@mi.unc.edu.ar>
2023-10-02 20:23:52 -0300

    First commit

---
+++ tpFinal.srcs/sources_1/new/top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.10.2023 20:21:51
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+
+    );
+endmodule
